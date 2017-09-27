91c42ba9f5197e1cc200d11b2a34e368cef3b608
Pawel <pawel.html@gmail.com>
2017-09-27 22:08:05 +0200

    added Navbar and Footer

--- app/App.js
+++ app/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
 
 class App extends React.Component {
     render(){
         return(
             <div>
+                < Navbar />
                 {this.props.children}
+                < Footer />
             </div>
         );
     }
